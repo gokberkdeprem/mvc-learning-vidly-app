6b96cd62a1ae2de7fef8b21227f06d10dcffb886
Gokberk Deprem <gokberkdeprem@alumni.sabanciuniv.edu>
2021-03-10 10:06:38 +0300

    Add customer and movies page with local harcoded database

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public ViewResult Index()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.ID == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer{ID = 1, Name =  "John Smith"},
+                new Customer{ID = 2,Name = "Marry Williams" }
+
+            };
+        }
+
+
+       
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { ID = 1, Name = "Shrek" },
+                new Movie { ID = 2, Name = "Wall-e" }
+            };
+        }
+
+        //GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+                {
+                    new Customer{Name = " Customer 1"},
+                    new Customer{Name = "Customer 2"}
+
+                };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(viewModel);
+
+            //***** For Education Purpose
+            //return Content("hello world");
+            //return HttpNotFound();
+            //return new EmptyResult();//retruns an empty page
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+
+        //public ActionResult Edit(int id)
+        //{
+        //    return Content("id=" + id);
+        //}
+
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+
+        //    if (!pageIndex.HasValue) pageIndex = 1;
+        //    if (string.IsNullOrWhiteSpace(sortBy)) sortBy = "name";
+
+        //    return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        //}
+
+        //public ActionResult ByReleaseDate(int year, int month)
+        //{
+
+        //    return Content(year + "/" + month);
+
+        //}
+
+    }
+}
